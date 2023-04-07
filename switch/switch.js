625e5b4355ca8a97694dc31fee850baa587b53bc
Mohibur Rahman <mohibur.rahman.sncf@gmail.com>
2023-04-07 10:47:27 +0200

    first commit

---
+++ switch/switch.js
@@ -0,0 +1,66 @@
+// Vowel or connsonant program using switch
+
+var letter = prompt("Enter a letter : ")
+letter = letter.toLowerCase()
+
+// checking the conditions for vowel and consonant
+switch (letter) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        document.write("Vowel");
+        break;
+    default:
+        document.write("Consonant");
+}
+
+
+// if input is not a number then program will display "not a digit"
+
+// var digit = parseInt(prompt("Enter a digit : "));
+// switch (digit) {
+//   case 0:
+//     document.write("Zero");
+//     break;
+
+//   case 1:
+//     document.write("One");
+//     break;
+
+//   case 2:
+//     document.write("Two");
+//     break;
+
+//   case 3:
+//     document.write("Three");
+//     break;
+
+//   case 4:
+//     document.write("Four");
+//     break;
+
+//   case 5:
+//     document.write("Five");
+//     break;
+
+//   case 6:
+//     document.write("Six");
+//     break;
+
+//   case 7:
+//     document.write("Seven");
+//     break;
+
+//   case 8:
+//     document.write("Eight");
+//     break;
+
+//   case 9:
+//     document.write("Nine");
+//     break;
+
+//   default:
+//     document.write("Not a digit");
+// }
